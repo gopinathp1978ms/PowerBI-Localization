53e107b8274e17a36132a2e2ef43c9ebe95854da
GOPINATH PANCHATCHARAM <81204938+gopinathp1978ms@users.noreply.github.com>
2021-03-23 19:44:05 -0700

    Add files via upload

--- ExtractLegendAndTitle.cs
+++ ExtractLegendAndTitle.cs
@@ -2,13 +2,15 @@ using System.Collections;
 using System.ComponentModel.Design;
 using System.Resources;
 
-var ReportName = "MakerActivity.Pbix";
-var resxTitleandLegendFilename = @"C:\gopip\pp\Admin-Analytics\HelperTool\files\PbiLocalizationResources\Resources\globaltitleandlegend.resx";
+var CategoryName = "PowerAutomate";
+var ReportName = "DailyUsage.Pbix";
+var Translated_filefolder = @"C:\..\PbixLocalization\Resources\";
+var resxTitleandLegendFilename = Translated_filefolder + "TitleandLegend.resx";
 // Reading existing keys to do update
 Hashtable resourceEntries = new Hashtable();
 
 foreach(var modelLanguage in Model.Cultures) {
-    var resxFilename = @"C:\gopip\pp\Admin-Analytics\HelperTool\files\PbiLocalizationResources\Resources\" + modelLanguage.Name + ".resx";
+    var resxFilename = Translated_filefolder + modelLanguage.Name + ".resx";
     
     using (ResXResourceReader reader = new ResXResourceReader(resxFilename) { UseResXDataNodes = true })
     {
@@ -16,11 +18,15 @@ foreach(var modelLanguage in Model.Cultures) {
         {
             if (resourceEntry.Key.ToString().Contains("Title") || resourceEntry.Key.ToString().Contains("Legend")){
                     ResXDataNode node = resourceEntry.Value as ResXDataNode;
-                    var translatedText = node.GetValue((ITypeResolutionService)null);
-                    var hashKey = modelLanguage.Name + "#" + resourceEntry.Key.ToString();
-                    // construct the node
-                    ResXDataNode nodeTitle = new ResXDataNode(hashKey, translatedText) { Comment = modelLanguage.Name };
-                    resourceEntries.Add(hashKey, nodeTitle);
+                    var comment = node.Comment;
+                    var commentArray = comment.Split('#');
+                    if (commentArray[0]== CategoryName && commentArray[1]== ReportName) {
+                        var translatedText = node.GetValue((ITypeResolutionService)null);
+                        var hashKey = modelLanguage.Name + "#" + resourceEntry.Key.ToString();
+                        // construct the node
+                        ResXDataNode nodeTitle = new ResXDataNode(hashKey, translatedText) { Comment = modelLanguage.Name };
+                        resourceEntries.Add(hashKey, nodeTitle);
+                    }
                 }
         }
     }
@@ -34,3 +40,4 @@ using (ResXResourceWriter resourceWriter = new ResXResourceWriter(resxTitleandLe
     }
     resourceWriter.Generate();
 }
+"Title and Legends are extracted to the file name TitleandLegend.resx".Output();
